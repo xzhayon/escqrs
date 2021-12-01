54e5d8a90847aab4b4f85bc747ffeb5a4d2ff3f2
Alberto Villa <alberto.villa@facile.it>
2021-12-01 19:52:45 +0100

    Add storage event store

--- src/Event.ts
+++ src/Event.ts
@@ -1,6 +1,5 @@
 import { Branded } from '@effect-ts/core'
-import { Entity } from '../src.bak/core/entities/Entity'
-import { Type } from './Entity'
+import { Entity, Type } from './Entity'
 import { $Message, Message } from './Message'
 
 export interface Event<A extends Entity = Entity, T extends string = string>
