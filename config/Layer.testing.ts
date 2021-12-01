b34c5d9de323c5cbf194a767a2530bfc7fab7883
Alberto Villa <alberto.villa@facile.it>
2021-12-01 15:53:41 +0100

    Add Storage and tests

--- config/Layer.testing.ts
+++ config/Layer.testing.ts
@@ -2,6 +2,7 @@ import { pipe } from '@effect-ts/core'
 import * as Layer from '@effect-ts/system/Layer'
 import EventEmitter from 'events'
 import { HasEventStore } from '../src/EventStore'
+import { $Fs } from '../src/Fs'
 import { $InMemoryEventStore } from '../src/InMemoryEventStore'
 import { $InMemoryRepository } from '../src/InMemoryRepository'
 import { $InMemoryServiceBus } from '../src/InMemoryServiceBus'
@@ -10,6 +11,9 @@ import { $NilLogger } from '../src/NilLogger'
 import { HasRepository } from '../src/Repository'
 import { $Rfc4122 } from '../src/Rfc4122'
 import { HasServiceBus } from '../src/ServiceBus'
+import { HasStorage } from '../src/Storage'
+import { fs } from 'memfs'
+import _fs from 'fs'
 import { HasUuid } from '../src/Uuid'
 
 export const $Layer = pipe(
@@ -21,6 +25,7 @@ export const $Layer = pipe(
     Layer.fromManaged(HasServiceBus)(
       $InMemoryServiceBus(() => new EventEmitter()),
     ),
+    Layer.pure(HasStorage)($Fs(fs as unknown as typeof _fs)),
     Layer.pure(HasUuid)($Rfc4122),
   ),
   Layer.usingAnd(Layer.pure(HasLogger)($NilLogger)),
---
+++ src/Storage.test.ts
@@ -0,0 +1,94 @@
+import { Effect, pipe } from '@effect-ts/core'
+import { gen } from '@effect-ts/system/Effect'
+import { $Layer } from '../config/Layer.testing'
+import { $Storage } from './Storage'
+
+describe('Storage', () => {
+  let seed = -1
+
+  beforeEach(() => {
+    seed++
+  })
+
+  it('reading a nonexistent file', async () => {
+    await expect(
+      pipe(
+        $Storage.read(`foo.${seed}`),
+        Effect.provideSomeLayer($Layer),
+        Effect.runPromise,
+      ),
+    ).rejects.toThrow()
+  })
+  it('writing to a file', async () => {
+    await expect(
+      pipe(
+        gen(function* (_) {
+          yield* _($Storage.write(`foo.${seed}`)(Buffer.from('bar')))
+
+          return yield* _($Storage.read(`foo.${seed}`))
+        }),
+        Effect.provideSomeLayer($Layer),
+        Effect.runPromise,
+      ),
+    ).resolves.toStrictEqual(Buffer.from('bar'))
+  })
+  it('writing to a nonexistent directory', async () => {
+    await expect(
+      pipe(
+        gen(function* (_) {
+          yield* _($Storage.write(`foo.${seed}/bar`)(Buffer.from('foobar')))
+
+          return yield* _($Storage.read(`foo.${seed}/bar`))
+        }),
+        Effect.provideSomeLayer($Layer),
+        Effect.runPromise,
+      ),
+    ).resolves.toStrictEqual(Buffer.from('foobar'))
+  })
+  it('replacing a file', async () => {
+    await expect(
+      pipe(
+        gen(function* (_) {
+          yield* _($Storage.write(`foo.${seed}/bar`)(Buffer.from('foo')))
+          yield* _($Storage.write(`foo.${seed}/bar`)(Buffer.from('bar')))
+
+          return yield* _($Storage.read(`foo.${seed}/bar`))
+        }),
+        Effect.provideSomeLayer($Layer),
+        Effect.runPromise,
+      ),
+    ).resolves.toStrictEqual(Buffer.from('bar'))
+  })
+  it('appending to a file', async () => {
+    await expect(
+      pipe(
+        gen(function* (_) {
+          yield* _($Storage.write(`foo.${seed}/bar`)(Buffer.from('foo')))
+          yield* _(
+            $Storage.write(`foo.${seed}/bar`, { append: true })(
+              Buffer.from('bar'),
+            ),
+          )
+
+          return yield* _($Storage.read(`foo.${seed}/bar`))
+        }),
+        Effect.provideSomeLayer($Layer),
+        Effect.runPromise,
+      ),
+    ).resolves.toStrictEqual(Buffer.from('foobar'))
+  })
+  it('deleting a file', async () => {
+    await expect(
+      pipe(
+        gen(function* (_) {
+          yield* _($Storage.write(`foo.${seed}/bar`)(Buffer.from('foobar')))
+          yield* _($Storage.delete(`foo.${seed}/bar`))
+
+          return yield* _($Storage.delete(`foo.${seed}/bar`))
+        }),
+        Effect.provideSomeLayer($Layer),
+        Effect.runPromise,
+      ),
+    ).rejects.toThrow()
+  })
+})
