998cd1dbf8ee7d981df2d7b83af91fc176def5a5
Alberto Villa <alberto.villa@facile.it>
2021-12-01 09:12:39 +0100

    Test Aggregate "apply"

--- src/Aggregate.test.ts
+++ src/Aggregate.test.ts
@@ -1,13 +1,15 @@
-import { Effect, pipe } from '@effect-ts/core'
+import { Branded, Effect, Either, pipe } from '@effect-ts/core'
 import { gen } from '@effect-ts/system/Effect'
 import { $Layer } from '../config/Layer.testing'
 import { $Aggregate } from './Aggregate'
-import { Body } from './Entity'
+import { Body, Header, Id } from './Entity'
 import { EntityNotFound } from './EntityNotFound'
 import { $Event, Event } from './Event'
 import { $EventSourcedEntity, EventSourcedEntity } from './EventSourcedEntity'
 import { $EventStore } from './EventStore'
+import { $MessageId } from './Message'
 import { $MutableEntity, MutableEntity } from './MutableEntity'
+import { PartialDeep } from './PartialDeep'
 import { $Repository } from './Repository'
 import { WrongEntityVersion } from './WrongEntityVersion'
 
@@ -17,9 +19,44 @@ interface Foo extends EventSourcedEntity<'foo'> {
 
 const aggregate = $Aggregate<Foo, Event>('foo', {
   foo: (entity, event) =>
-    entity
-      ? { ...entity, bar: entity.bar + ((event as any).bar ?? 0) }
-      : { bar: (event as any).bar },
+    'bar' in event
+      ? entity
+        ? { ...entity, bar: entity.bar + ((event as any).bar ?? 0) }
+        : { bar: (event as any).bar }
+      : undefined,
+})
+
+const event = (
+  aggregateId: Event['aggregateId'],
+  body?: object,
+  header?: PartialDeep<Header<Event>>,
+): Event =>
+  Branded.makeBranded({
+    _: {
+      type: 'type',
+      id: $MessageId('id'),
+      date: new Date(),
+      correlationId: $MessageId('correlationId'),
+      causationId: $MessageId('causationId'),
+      ...header,
+    },
+    aggregateId,
+    ...body,
+  })
+
+const entity = (
+  id: Id<EventSourcedEntity>,
+  header?: PartialDeep<Header<EventSourcedEntity>>,
+): Foo => ({
+  _: {
+    ...header,
+    type: 'foo',
+    id,
+    date: { created: new Date(), updated: new Date(), ...header?.date },
+    version: -1,
+    events: { uncommitted: [] },
+  },
+  bar: 0,
 })
 
 describe('Aggregate', () => {
@@ -40,6 +77,20 @@ describe('Aggregate', () => {
           Effect.runPromise,
         ),
       ).rejects.toThrow(EntityNotFound)
+    })
+    it('failing when the events nullify the entity', async () => {
+      await expect(
+        pipe(
+          gen(function* (_) {
+            const event = yield* _($Event('foo')({ aggregateId: 'bar' })())
+            yield* _($EventStore.publish(event))
+
+            return yield* _(aggregate.load('bar'))
+          }),
+          Effect.provideSomeLayer($Layer),
+          Effect.runPromise,
+        ),
+      ).rejects.toThrow(EntityNotFound.unreducibleEvents('foo', 'bar'))
     })
     it('failing when loading an entity of the wrong kind', async () => {
       await expect(
@@ -101,6 +152,60 @@ describe('Aggregate', () => {
           Effect.runPromise,
         ),
       ).resolves.toMatchObject({ _: { type: 'foo', id: 'bar' }, bar: 42 })
+    })
+  })
+
+  describe('apply', () => {
+    it('applying an event to a new entity', () => {
+      const _event = event('foo', { bar: 42 })
+
+      expect(aggregate.apply(_event)()).toStrictEqual(
+        Either.right({
+          _: {
+            type: 'foo',
+            id: 'foo',
+            date: { created: _event._.date, updated: _event._.date },
+            version: -1,
+            events: { uncommitted: [_event] },
+          },
+        }),
+      )
+    })
+    it('applying an event to an existing entity', () => {
+      const _event = event('bar', { bar: 42 })
+      const _entity = entity('bar')
+
+      expect(aggregate.apply(_event)(_entity)).toStrictEqual(
+        Either.right({
+          _: {
+            ..._entity._,
+            date: { ..._entity._.date, updated: _event._.date },
+            events: { uncommitted: [_event] },
+          },
+        }),
+      )
+    })
+    it('applying an event that nullifies the entity', () => {
+      const _event = event('bar')
+      const _entity = entity('bar')
+
+      expect(aggregate.apply(_event)(_entity)).toStrictEqual(
+        Either.right({
+          _: {
+            ..._entity._,
+            date: { ..._entity._.date, updated: _event._.date },
+            events: { uncommitted: [_event] },
+          },
+        }),
+      )
+    })
+    it('applying an event of another entity to an existing one', () => {
+      const _event = event('foo')
+      const _entity = entity('bar')
+
+      expect(aggregate.apply(_event)(_entity)).toStrictEqual(
+        Either.right({ _: _entity._ }),
+      )
     })
   })
 
