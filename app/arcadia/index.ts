693213662c0b0b148167dc92546a5d2c18a1b7a7
Alberto Villa <alberto.villa@facile.it>
2021-12-03 14:33:20 +0100

    Install Fastify

--- app/arcadia/index.ts
+++ app/arcadia/index.ts
@@ -1,16 +1,16 @@
-import { Effect, pipe } from '@effect-ts/core'
-import { gen } from '@effect-ts/system/Effect'
-import { $Layer } from '../../config/Layer.local'
-import { $Event } from '../../src/Event'
-import { $EventStore } from '../../src/EventStore'
-import { $Uuid } from '../../src/Uuid'
+import _fastify from 'fastify'
 
-pipe(
-  gen(function* (_) {
-    const aggregateId = yield* _($Uuid.v4)
-    const event = yield* _($Event('foo')({ aggregateId })())
-    yield* _($EventStore.publish(event))
-  }),
-  Effect.provideSomeLayer($Layer),
-  Effect.run,
-)
+const fastify = _fastify()
+
+fastify.get('/', async () => 'hello, world')
+
+const start = async () => {
+  try {
+    await fastify.listen(process.argv[2] ?? 0)
+  } catch (error) {
+    fastify.log.error(error)
+    throw error
+  }
+}
+
+start()
