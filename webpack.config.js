e08edc0a16b4122e6764bf7a7306e60572e69496
Alberto Villa <alberto.villa@facile.it>
2021-12-04 11:23:40 +0100

    Use .env to configure Docker

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = (env, argv) => ({
     },
   },
   plugins: [
-    new DotEnv({ path: './.env.local' }),
+    new DotEnv(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
       inject: 'body',
