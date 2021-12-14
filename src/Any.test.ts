dec792f394cd20ccd7af141fe31fdf1770c2c99d
Alberto Villa <alberto.villa@facile.it>
2021-12-14 10:07:10 +0100

    Add codecs

--- src/Any.test.ts
+++ src/Any.test.ts
@@ -3,6 +3,42 @@ import * as t from 'io-ts'
 import { $Any } from './Any'
 
 describe('Any', () => {
+  describe('bit', () => {
+    test.each([0 as const, 1 as const])(
+      'decoding and encoding successfully (%p)',
+      (value) => {
+        expect($Any.decode($Any.bit)(value)).toStrictEqual(Either.right(value))
+        expect($Any.bit.encode(value)).toStrictEqual(value)
+      },
+    )
+    test.each([undefined, false, true, '0', '1', 42])(
+      'failing decoding (%p)',
+      (value) => {
+        expect($Any.decode($Any.bit)(value)).toBeInstanceOf(Either.Left)
+      },
+    )
+  })
+
+  describe('booleanFromBit', () => {
+    test.each([
+      [0, false],
+      [1, true],
+    ])('decoding and encoding successfully (%p)', (value, decoded) => {
+      expect($Any.decode($Any.booleanFromBit)(value)).toStrictEqual(
+        Either.right(decoded),
+      )
+      expect($Any.booleanFromBit.encode(decoded)).toStrictEqual(value)
+    })
+    test.each([undefined, false, true, '0', '1', 42])(
+      'failing decoding (%p)',
+      (value) => {
+        expect($Any.decode($Any.booleanFromBit)(value)).toBeInstanceOf(
+          Either.Left,
+        )
+      },
+    )
+  })
+
   describe('decode', () => {
     test('decoding successfully', () => {
       expect($Any.decode(t.boolean)(true)).toStrictEqual(Either.right(true))
--- src/Any.ts
+++ src/Any.ts
@@ -1,8 +1,26 @@
 import { Either } from '@effect-ts/core'
-import { either, monoid, string } from 'fp-ts'
+import { boolean, either, monoid, string } from 'fp-ts'
 import { pipe } from 'fp-ts/function'
 import * as t from 'io-ts'
 import { failure } from 'io-ts/PathReporter'
+
+const bit = t.union([t.literal(0), t.literal(1)], 'Bit')
+
+const booleanFromBit = new t.Type(
+  'BooleanFromBit',
+  t.boolean.is,
+  (u, c) =>
+    pipe(
+      u,
+      bit.decode,
+      either.map((b) => (0 === b ? false : true)),
+      either.match(() => t.failure(u, c), t.success),
+    ),
+  boolean.match(
+    () => 0 as const,
+    () => 1 as const,
+  ),
+)
 
 const decode =
   <C extends t.Mixed>(codec: C) =>
@@ -16,4 +34,4 @@ const decode =
       either.matchW(Either.left, Either.right),
     )
 
-export const $Any = { decode }
+export const $Any = { bit, booleanFromBit, decode }
