614c049acd291d6b5d7aa4f96f33c9f6b0ee8205
Alberto Villa <alberto.villa@facile.it>
2021-12-16 01:12:22 +0100

    Complete testing configuration

--- config/Layer.testing.ts
+++ config/Layer.testing.ts
@@ -1,6 +1,7 @@
 import { pipe } from '@effect-ts/core'
 import * as Layer from '@effect-ts/system/Layer'
 import EventEmitter from 'events'
+import fastify from 'fastify'
 import _fs from 'fs'
 import { fs } from 'memfs'
 import { $InMemoryServiceBus } from '../src/entity/message/command/servicebus/InMemoryServiceBus'
@@ -9,6 +10,8 @@ import { HasEventStore } from '../src/entity/message/event/eventstore/EventStore
 import { $InMemoryEventStore } from '../src/entity/message/event/eventstore/InMemoryEventStore'
 import { $InMemoryRepository } from '../src/entity/repository/InMemoryRepository'
 import { HasRepository } from '../src/entity/repository/Repository'
+import { $FastifyHttpServer } from '../src/http/server/FastifyHttpServer'
+import { HasHttpServer } from '../src/http/server/HttpServer'
 import { HasLogger } from '../src/logger/Logger'
 import { $NilLogger } from '../src/logger/NilLogger'
 import { $Fs } from '../src/storage/Fs'
@@ -21,6 +24,7 @@ export const $Layer = pipe(
     Layer.fromManaged(HasEventStore)(
       $InMemoryEventStore(() => new EventEmitter()),
     ),
+    Layer.fromManaged(HasHttpServer)($FastifyHttpServer(fastify)),
     Layer.fromManaged(HasRepository)($InMemoryRepository),
     Layer.fromManaged(HasServiceBus)(
       $InMemoryServiceBus(() => new EventEmitter()),
